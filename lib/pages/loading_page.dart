b379b2e5f787f2eac810e8cb5c298bdcac78652a
rique <henrique.oliveira.gomes@gmail.com>
2020-04-05 12:22:12 -0300

    LoadingPage allowed

---
+++ lib/pages/loading_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class LoadingPage extends StatefulWidget {
+  LoadingPage({Key key}) : super(key: key);
+
+  @override
+  _LoadingPageState createState() => _LoadingPageState();
+}
+
+class _LoadingPageState extends State<LoadingPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(const Duration(seconds: 10),(){
+      initiateApp();
+    });
+  }
+
+  void initiateApp(){
+    Navigator.pushNamed(context, '/login');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitFadingCircle(
+          color: Colors.white,
+          size: 200.0,
+        ),
+      ),
+    );
+  }
+}
