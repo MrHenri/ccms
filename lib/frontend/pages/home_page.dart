17e46f3a1c22afe062d603271e681c2ac40fc519
BiaHawk <48568610+BiaHawk@users.noreply.github.com>
2020-04-26 22:23:48 -0300

    Update home_page.dart

--- lib/frontend/pages/home_page.dart
+++ lib/frontend/pages/home_page.dart
@@ -9,12 +9,119 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
+final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Home", style: TextStyle(color: Colors.blueGrey),),
+      key: _scaffoldKey,
+      drawer: Drawer(
+        child: ListView(
+          children: <Widget>[
+            ListTile(
+              title: Text("Perfil"),
+              leading: Builder(
+                builder: (BuildContext context){
+                  return IconButton(
+                    icon: Icon(IconData(59475, fontFamily: 'MaterialIcons'),),
+                    onPressed: () {_scaffoldKey.currentState.openDrawer();},
+                  );
+                },
+              ),
+
+            ),
+            ListTile(
+              title: Text("Escala"),
+              leading: Builder(
+                builder: (BuildContext context){
+                  return IconButton(
+                    icon: Icon(IconData(59555, fontFamily: 'MaterialIcons'),),
+                    onPressed: () {_scaffoldKey.currentState.openDrawer();},
+                  );
+                },
+              ),
+
+            ),
+            ListTile(
+              title: Text("Edição de grupo"),
+              leading: Builder(
+                builder: (BuildContext context){
+                  return IconButton(
+                    icon: Icon(IconData(59376, fontFamily: 'MaterialIcons'),),
+                    onPressed: () {_scaffoldKey.currentState.openDrawer();},
+                  );
+                },
+              ),
+
+            ),
+            ListTile(
+              title: Text("Protocolo"),
+              leading: Builder(
+                builder: (BuildContext context){
+                  return IconButton(
+                    icon: Icon(IconData(60219, fontFamily: 'MaterialIcons'),),
+                    onPressed: () {_scaffoldKey.currentState.openDrawer();},
+                  );
+                },
+              ),
+
+            ),
+            ListTile(
+              title: Text("Treinamentos"),
+              leading: Builder(
+                builder: (BuildContext context){
+                  return IconButton(
+                    icon: Icon(IconData(57444, fontFamily: 'MaterialIcons'),),
+                    onPressed: () {_scaffoldKey.currentState.openDrawer();},
+                  );
+                },
+              ),
+
+            ),
+            ListTile(
+              title: Text("Relatório"),
+              leading: Builder(
+                builder: (BuildContext context){
+                  return IconButton(
+                    icon: Icon(IconData(59485, fontFamily: 'MaterialIcons', matchTextDirection: true),),
+                    onPressed: () {_scaffoldKey.currentState.openDrawer();},
+                  );
+                },
+              ),
+
+            ),
+            ListTile(
+              title: Text("Serviços"),
+              leading: Builder(
+                builder: (BuildContext context){
+                  return IconButton(
+                    icon: Icon(IconData(58905, fontFamily: 'MaterialIcons'),),
+                    onPressed: () {_scaffoldKey.currentState.openDrawer();},
+                  );
+                },
+              ),
+
+            ),
+          ],
+        ),
+      ),
+      body: Stack(
+        children: <Widget>[
+          new Center(
+            child: new Column(
+              children: <Widget>[],
+            ),
+          ),
+          Positioned(
+            left: 10,
+            top: 20,
+            child: IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () => _scaffoldKey.currentState.openDrawer(),
+            ),
+          ),
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
